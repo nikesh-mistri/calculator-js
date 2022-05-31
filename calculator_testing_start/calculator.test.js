42d690ccc53026287e1c741ef4ba0295072e7fd7
Nikesh Mistri <nikeshmistri@hotmail.co.uk>
2022-05-31 15:43:19 +0100

    tests for subtracts

--- calculator_testing_start/calculator.test.js
+++ calculator_testing_start/calculator.test.js
@@ -16,20 +16,20 @@ describe('sum', () => {
     expect(actual).toBe(expected);
   });
 
-  test.skip('can add two large positive numbers', () => { 
+  test('can add two large positive numbers', () => { 
     expected = 10000
     actual = sum(3000, 7000);
     expect(actual).toBe(expected);
     
   });
 
-  test.skip('can add two negative numbers', () => {
+  test('can add two negative numbers', () => {
     expected = -9;
     actual = sum(-4, -5);
     expect(actual).toBe(expected);
   });
 
-  test.skip('can add zero', () => {
+  test('can add zero', () => {
     expected = 8;
     actual = sum(8, 0);
     expect(actual).toBe(expected);
@@ -38,6 +38,30 @@ describe('sum', () => {
 });
 
 describe('subtract', () => {
+
+  test('can subtract two small positive numbers', () => {
+    expected = 3;
+    actual = subtract(9,6);
+    expect(actual).toBe(expected);
+  })
+
+  test('can subtract two large positive numbers', () => {
+    expected = 500000;
+    actual = subtract(1000000, 500000);
+    expect(actual).toBe(expected);
+  })
+
+  test('can subtract two negative numbers', () => {
+    expected = -2;
+    actual = subtract(-6,-4);
+    expect(actual).toBe(expected);
+  })
+
+  test('can subtract zero', () => {
+    expected = 9;
+    actual = subtract(9, 0);
+    expect(actual).toBe(expected);
+  })
 
 });
 
